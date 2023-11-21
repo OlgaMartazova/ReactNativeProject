bc4fc422ed420823d3c4af8130568ae6e2d131bb
Olga Martazova <71041400+OlgaMartazova@users.noreply.github.com>
2023-11-21 17:34:01 +0300

    First commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View, Button } from 'react-native';
+
+
+export default function HomeScreen({ }) {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
---
+++ screens/NewsScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+
+export default function NewsScreen({ }) {
+    return (
+        <View>
+            <Text>News</Text>
+        </View>
+    )
+}
