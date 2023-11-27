f3820e56b8f61d0234e450a0d9fa4168d531251d
Olga Martazova <71041400+OlgaMartazova@users.noreply.github.com>
2023-11-28 01:51:50 +0300

    deep linking to about

---
+++ App.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { View, Text, Button, Linking } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SettingsScreen from "./screens/SettingsScreen";
+import NewsScreen from "./screens/NewsScreen";
+import ChatScreen from "./screens/ChatScreen";
+import HomeScreen from "./screens/HomeScreen";
+import Navigation from "./navigation/Navigation";
+import { DeepLinking } from "./navigation/DeepLinking";
+import AboutScreen from "./screens/AboutScreen";
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+const TabNavigation = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeStack} />
+            <Tab.Screen name="News" component={NewsScreen} />
+            <Tab.Screen name="Chat" component={ChatScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    );
+}
+const HomeStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name={'HomeScreen'}
+                component={HomeScreen}
+                options={(props) => ({
+                    headerRight: () => (
+                        <Button
+                            onPress={() => props.navigation.navigate('About')}
+                            title="О приложении"
+                            color="#000"
+                        />)
+                })}
+            />
+            <Stack.Screen name={'About'} component={AboutScreen} initialParams={{ itemId: 42 }} />
+        </Stack.Navigator>
+    );
+}
+
+const App = () => {
+    useEffect(() => {
+        Linking.getInitialURL().then(async (deepLinkInitialURL: any) => {
+            if (deepLinkInitialURL) {
+                await DeepLinking.handleInitialNavigate(deepLinkInitialURL);
+            }
+        });
+    }, []);
+
+    return (
+        <NavigationContainer
+            linking={DeepLinking.linking}
+            ref={Navigation.navigationRef}>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name={'Tab'}
+                    component={TabNavigation}
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
